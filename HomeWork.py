03f4308cf50d1d10b7492d5a275f685c5a3d0e62
LiuZhongYg <1690490917@qq.com>
2018-10-20 23:08:19 +0800

    2

--- HomeWork.py
+++ HomeWork.py
@@ -1,6 +1,8 @@
 import os
+import nltk
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import WordPunctTokenizer
+from nltk.corpus import stopwords
 porter_stemmer = PorterStemmer()
 def wordtokenizer(sentence):
     word =WordPunctTokenizer().tokenize(sentence)
@@ -8,6 +10,9 @@ def wordtokenizer(sentence):
 
 path="C:\\Users\\lzy\\Desktop\\20news-18828\\20news-18828"
 dirs=os.listdir(path)
+file=[]          #
+vocabulary=[]    #词汇表
+table={}         #统计各文本单词的频率
 for file in dirs:
     dirs2=os.listdir(path+"\\"+file)
     for file2 in dirs2:
@@ -15,5 +20,6 @@ for file in dirs:
         f=open(path+"\\"+file+"\\"+file2,errors='ignore')
         word=wordtokenizer(f.read())
         for w in word:
-            word[i]=porter_stemmer.stem(word[i]) 
-        
+            word[i]=porter_stemmer.stem(word[i]) #normalization
+        filtered=[w for w in word if(w not in stopwords.words('english'))] #remove stopwords
+      
