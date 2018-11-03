8f5ea4a012286dac68f67c16c4bb8690b6dff44f
LiuZhongYg <1690490917@qq.com>
2018-11-03 20:13:16 +0800

    correct

--- vsm.py
+++ vsm.py
@@ -39,8 +39,8 @@ def data_process1(path):
                     dic[w]+=1
                 else:
                     dic[w]=1
-##                if w not in vocabulary:
-##                    vocabulary.append(w)  #初步建立词汇表
+##                if w not in vocabularies:
+##                    vocabularies.append(w)  #初步建立词汇表
                 log+=1
                 if log%1000==0:
                     print(log)
@@ -50,8 +50,8 @@ def data_process1(path):
 ##            with open(path2+"/"+file+"/"+file_txt,'w+') as f:   
 ##                for k,v in dic.items():
 ##                    f.write(k+":"+str(v)+"\n")                      
-##    with open("./vocabulary.txt",'w+') as f:
-##        for i in vocabulary:
+##    with open("./vocabularies.txt",'w+') as f:
+##        for i in vocabularies:
 ##            f.write(str(i)+"\n")
     return list
 ##def data_stem_if(path):
@@ -114,8 +114,8 @@ def data_process2(data,w_min):
     "数据来自data_stem_if()，计算idf,求得权重，过滤词典"
     table={}
     data1=data
-    vocabulary=[]#修改
-    with open("./vocabulary.txt",'r') as f:
+    vocabularies=[]#修改
+    with open("./vocabularies.txt",'r') as f:
         data_con=f.readlines()
         data_con=TextBlob(str(data_con).replace("\\n","").replace("'","").replace("\\t","").replace("\\",""))
         data_con=data_con.words
@@ -131,18 +131,18 @@ def data_process2(data,w_min):
         for k,v in file.items():                 
             if table.get(k,0)!=0:
                 file[k]=v*math.log(len(data)/table[k])
-                if file[k]>=w_min and (k not in vocabulary):
-                    vocabulary.append(k)
+                if file[k]>=w_min and (k not in vocabularies):
+                    vocabularies.append(k)
        
     index=0    
-    with open("./vocabulary.txt",'w+') as f:
-        for i in vocabulary:
+    with open("./vocabularies.txt",'w+') as f:
+        for i in vocabularies:
             f.write(str(i)+"\n")
             index+=1
     print(index)
-    return vocabulary,data
+    return vocabularies,data
 
-def create_vsm(path,data,vocabulary):
+def create_vsm(path,data,vocabularies):
     "建立向量空间模型"
     count=0
     files=os.listdir(path)
@@ -150,7 +150,7 @@ def create_vsm(path,data,vocabulary):
         file_list=os.listdir(path+ "/" +file)
         for file_txt in file_list:
             with open(path + "/" + file + "/" + file_txt,'w+') as f:
-                for word in vocabulary:
+                for word in vocabularies:
                     if data[count].get(word,0)!=0:
                         f.write(word+":"+str(data[count][word])+"\n")
                     else:
